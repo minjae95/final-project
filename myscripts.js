21a1a384d88145975e0d21c3b11ed257318b1d25
minjae95 <53993514+minjae95@users.noreply.github.com>
2022-07-10 20:08:52 +1000

    updated

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,29 @@
 // create some modals for about us and contact us
 //
+const id = document.querySelector("#username");
+const pw = document.querySelector("#password");
 
-function outOfStock() {
-  document.querySelector();
-}
+document.querySelectorAll(".open").forEach((element) =>
+  element.addEventListener("click", function () {
+    document.querySelector("#overlay").classList.add("display");
+  })
+);
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector("#overlay").classList.remove("display");
+});
+
+document.querySelector(".signin-bar").addEventListener("click", function () {
+  document.querySelector(".modal-content").style.display = "block";
+});
+
+document.querySelector(".close-btn").addEventListener("click", function () {
+  document.querySelector(".modal-content").style.display = "none";
+});
+
+document.querySelector(".search-btn").addEventListener("click", function () {
+  document.querySelector(".search-modal-content").style.display = "block";
+});
+
+document.querySelector(".search-close").addEventListener("click", function () {
+  document.querySelector(".search-modal-content").style.display = "none";
+});
